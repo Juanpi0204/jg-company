3c77f751f72c020c0d68c6fbf5da7108fb3c409d
Juan Garces <juan.garces028@gmail.com>
2026-09-22 22:58:54 -0500

    deploy: codificacion limpia de URI para WhatsApp sin doble porcentaje

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2797826096" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2719208373" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
